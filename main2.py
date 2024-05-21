bd375ce919af4b195f984fac0aee02fca1aa5b31
rromero <rromero@heligrafic.net>
2024-05-21 12:39:11 -0400

    add: create conexion gcp

---
+++ main2.py
@@ -0,0 +1,9 @@
+import os
+from google.cloud import storage
+
+
+client = storage.Client.from_service_account_json("C:\DEV\python\mineria-datos\credencial\entrega2-424015-9aeaeac36b69.json")
+
+buckets = client.list_buckets()
+for bucket in buckets:
+    print(bucket.name)
